4b84f3c7a85adce10d725425adee0653e82f958b
Jhoana Pinto <jhoana.pinto@unosquare.com>
2022-07-11 11:15:14 -0500

    deleted keys

--- 1.6-NodeJS-AdvancedConcepts/AdvancedNodeStarter/config/dev.js
+++ 1.6-NodeJS-AdvancedConcepts/AdvancedNodeStarter/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+    '',
   googleClientSecret: '',
   mongoURI: 'mongodb://jhoana:jFFII27w4BPYgTBl@cluster0-shard-00-00.ntpvn.mongodb.net:27017,cluster0-shard-00-01.ntpvn.mongodb.net:27017,cluster0-shard-00-02.ntpvn.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-3reo4z-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
